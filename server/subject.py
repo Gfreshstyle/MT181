4e62b2b8986f8197086e792d8fa64421a724a847
Mikee Villanueva <mikeeevillanueva@gmail.com>
2016-11-08 23:52:45 +0800

    update

--- server/subject.py
+++ server/subject.py
@@ -6,10 +6,12 @@ def Subject():
     if username = "Buloy":
         list = [
             {"subject": "CSC101", "time": "7:30-9:00", "day": "MTH"}
-
         ]
-
-
+    if username = "Kalay":
+         list =  [ 
+             {"subject": "FIL1", "time": "7:30-9:00", "day": "TF"}
+         ]
+            
     return jsonify(results=list)
 
 if __name__ == '__main__':
